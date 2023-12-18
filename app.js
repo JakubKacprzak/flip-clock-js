09fd55fecbfa961582a0909029a5d6f19c9e6f6f
Jakub Kacprzak <jkacprzak263@gmail.com>
2023-12-18 16:17:05 +0100

    JS functionality before aniamtion

--- app.js
+++ app.js
@@ -3,17 +3,40 @@ function flipClock(){
   const currentMinute = document.querySelector('.current-minute');
   const currentSecond = document.querySelector('.current-second');
 
+  const nextHour = document.querySelector('.next-hour');
+  const nextMinute = document.querySelector('.next-minute');
+  const nextSecond = document.querySelector('.next-second');
+
+
   const timeDelay = 1000;
+  let currHrs;
+  let currMin;
+  let currSec;
+  let nextHrs;
+  let nextMin;
+  let nextSec;
+  let maxTime = 59;
 
   const displayTime = () => {
     const date = new Date();
-    let hrs = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
 
-    currentHour.innerHTML = hrs > 9 ? hrs : "0" + hrs;
-    currentMinute.innerHTML = min > 9 ? min : "0" + min;
-    currentSecond.innerHTML = sec > 9 ? sec : "0" + sec;  
+    currHrs = date.getHours();
+    currMin = date.getMinutes();
+    currSec = date.getSeconds();
+
+    nextHrs = currHrs < maxTime ? currHrs + 1: 0;
+    nextMin = currMin < maxTime ? currMin + 1: 0;
+    nextSec = currSec < maxTime ? currSec + 1: 0;
+
+
+
+    currentHour.innerHTML = currHrs > 9 ? currHrs : "0" + currHrs;
+    currentMinute.innerHTML = currMin > 9 ? currMin : "0" + currMin;
+    currentSecond.innerHTML = currSec > 9 ? currSec : "0" + currSec;
+    
+    nextHour.innerHTML = nextHrs > 9 ? nextHrs : "0" + nextHrs;
+    nextMinute.innerHTML = nextMin > 9 ? nextMin : "0" + nextMin;
+    nextSecond.innerHTML = nextSec > 9 ? nextSec : "0" + nextSec;
   }
 
   setInterval(displayTime, timeDelay);
